e802cb9ddcaa26b96f3cf828b1cfde1be64ca5d0
Ruslan Smirnov <smirnov_ruslan@outlook.com>
2021-07-05 23:35:45 +0300

    Add losses

--- source.py
+++ source.py
@@ -18,6 +18,37 @@ DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 IMAGE_SIZE = 512 if torch.cuda.is_available() else 128
 
 
+class ContentLoss(nn.Module):
+    def __init__(self, target):
+        super(ContentLoss, self).__init__()
+        self.target = target.detach()
+
+    def forward(self, input):
+        self.loss = F.mse_loss(input, self.target)
+        return input
+
+
+def gram_matrix(input):
+    a, b, c, d = input.size()
+    features = input.view(a * b, c * d)
+
+    G = torch.mm(features, features.t())
+
+    return G.div(a * b * c * d)
+
+
+class StyleLoss(nn.Module):
+    def __init__(self, target_feature):
+        super(StyleLoss, self).__init__()
+        self.target = gram_matrix(target_feature).detach()
+
+    def forward(self, input):
+        G = gram_matrix(input)
+        self.loss = F.mse_loss(G, self.target)
+
+        return input
+
+
 def image_loader_init(loader):
     loader = loader
 
